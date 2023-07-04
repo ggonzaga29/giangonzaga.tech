9cdd77cc43f432aa3cdec5b0284b45e80273e0dd
Gian Gonzaga <gonzagagianfrancesco@gmail.com>
2023-07-05 03:13:57 +0800

    seperate drawer into component

---
+++ src/components/drawer/Drawer.tsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Button } from '../ui/button';
+import { X } from 'lucide-react';
+import { nanoid } from 'nanoid';
+
+type DrawerProps = {
+	isSidebarOpen: boolean;
+	setIsSidebarOpen: React.Dispatch<React.SetStateAction<boolean>>;
+	currentRoute: string;
+	pages: {
+		name: string;
+		path: string;
+	}[];
+};
+
+const Drawer = ({
+	isSidebarOpen,
+	setIsSidebarOpen,
+	currentRoute,
+	pages,
+}: DrawerProps) => {
+	return (
+		<>
+			{/* drawer */}
+			<div
+			// className={`${isSidebarOpen ? '' : 'hidden'} md:hidden`}
+			>
+				{/* overlay */}
+				<div
+					className={`absolute w-full h-full inset-0 bg-black bg-opacity-50 z-50 transition-all duration-200 ${
+						isSidebarOpen ? '' : 'opacity-0 pointer-events-none'
+					}`}
+					onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+				/>
+				<div
+					className={`absolute w-2/3 left-0 top-0 bg-white dark:bg-zinc-950 z-50 h-full transition-all duration-200 ${
+						isSidebarOpen ? '' : 'transform -translate-x-full'
+					}`}
+				>
+					{/* header */}
+					<div className='flex justify-between items-center px-2 py-2'>
+						<h1 className='font-bold text-3xl'>
+							<Link href='/'>
+								<Image
+									src='/logo.png'
+									width={64}
+									height={64}
+									alt='logo'
+									className='dark:invert'
+								/>
+							</Link>
+						</h1>
+						<Button
+							variant='outline'
+							size='icon'
+							className={`flex items-center justify-center absolute left-[110%] delay-100 transition-all duration-50 ${
+								isSidebarOpen
+									? ''
+									: 'transform -translate-y-[120%] opacity-0'
+							}`}
+							onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+						>
+							<X className='h-6 w-6' />
+						</Button>
+					</div>
+					{/* end header */}
+					<div className='flex flex-col mt-3'>
+						{pages.map((page) => (
+							<Link
+								href={page.path}
+								key={nanoid()}
+								className={`${
+									currentRoute === page.path
+										? 'bg-gray-100 hover:bg-gray-200 dark:bg-zinc-800 dark:hover:bg-zinc-700'
+										: ''
+								} transition-all duration-300 px-4 py-4`}
+							>
+								{page.name}
+							</Link>
+						))}
+					</div>
+				</div>
+			</div>
+			{/* drawer end */}
+		</>
+	);
+};
+
+export default Drawer;
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -11,6 +11,10 @@ import { Button } from '../ui/button';
 
 import { SidebarOpen, SidebarClose, Menu, X } from 'lucide-react';
 
+import pages from '@/data/pages';
+
+import Drawer from '../drawer/Drawer';
+
 const Navbar = () => {
 	const currentRoute = usePathname();
 	const [isSidebarOpen, setIsSidebarOpen] = useState(false);
@@ -22,35 +26,17 @@ const Navbar = () => {
 	useEffect(() => {
 		scrollTo(0, 0);
 
-			if (isSidebarOpen) {
-				document.body.style.overflow = 'hidden';
-			} else {
-				document.body.style.overflow = 'unset';
-			}
+		if (isSidebarOpen) {
+			document.body.style.overflow = 'hidden';
+		} else {
+			document.body.style.overflow = 'unset';
+		}
 	}, [isSidebarOpen]);
-
-	const pages = [
-		{
-			name: 'Home',
-			path: '/',
-		},
-		{
-			name: 'Portfolio',
-			path: '/portfolio',
-		},
-		{
-			name: 'Contact',
-			path: '/contact',
-		},
-		{
-			name: 'Blog',
-			path: '/blog',
-		},
-	];
 
 	return (
 		<div className='h-16 mt-2 md:mt-10 select-none'>
 			<div className='md:max-w-[67%] mx-auto md:px-0 px-4 h-full flex items-center justify-between'>
+				{/* Logo */}
 				<div>
 					<h1 className='font-bold text-3xl '>
 						<Link href='/'>
@@ -64,6 +50,9 @@ const Navbar = () => {
 						</Link>
 					</h1>
 				</div>
+				{/* End Logo */}
+
+				{/* Navigation Menu */}
 				<div className='flex gap-5 cursor-pointer items-center'>
 					<div className='space-x-5 md:block hidden'>
 						{pages.map((page) => (
@@ -80,62 +69,19 @@ const Navbar = () => {
 							</Link>
 						))}
 					</div>
-
-					{/* drawer */}
-					<div
-						// className={`${isSidebarOpen ? '' : 'hidden'} md:hidden`}
-					>
-						{/* overlay */}
-						<div
-							className={`absolute w-full h-full inset-0 bg-black bg-opacity-50 z-50 transition-all duration-200 ${isSidebarOpen ? '' : 'opacity-0 pointer-events-none'}`}
-							onClick={() => setIsSidebarOpen(!isSidebarOpen)}
-						/>
-						<div className={`absolute w-2/3 left-0 top-0 bg-white dark:bg-zinc-950 z-50 h-full transition-all duration-200 ${
-							isSidebarOpen ? '' : 'transform -translate-x-full'
-						}`}>
-							{/* header */}
-							<div className='flex justify-between items-center px-2 py-2'>
-								<h1 className='font-bold text-3xl'>
-									<Link href='/'>
-										<Image
-											src='/logo.png'
-											width={64}
-											height={64}
-											alt='logo'
-											className='dark:invert'
-										/>
-									</Link>
-								</h1>
-								<Button
-									variant='outline'
-									size='icon'
-									className={`flex items-center justify-center absolute left-[110%] delay-100 transition-all duration-50 ${isSidebarOpen ? '' : 'transform -translate-y-[120%] opacity-0'}`}
-									onClick={() => setIsSidebarOpen(!isSidebarOpen)}
-								>
-									<X className='h-6 w-6' />
-								</Button>
-							</div>
-							{/* end header */}
-							<div className='flex flex-col mt-3'>
-								{pages.map((page) => (
-									<Link
-										href={page.path}
-										key={nanoid()}
-										className={`${
-											currentRoute === page.path
-												? 'bg-gray-100 hover:bg-gray-200 dark:bg-zinc-800 dark:hover:bg-zinc-700'
-												: ''
-										} transition-all duration-300 px-4 py-4`}
-									>
-										{page.name}
-									</Link>
-								))}
-							</div>
-						</div>
-					</div>
-					{/* drawer end */}
 				</div>
+				{/* End Navigation Menu */}
 
+				{/* Mobile Drawer */}
+				<Drawer
+					isSidebarOpen={isSidebarOpen}
+					setIsSidebarOpen={setIsSidebarOpen}
+					currentRoute={currentRoute}
+					pages={pages}
+				/>
+				{/* End Mobile Drawer */}
+
+				{/* Mobile Menu and Theme Toggle */}
 				<div className='flex gap-2 justify-center items-center'>
 					<ModeToggle />
 					<Button
@@ -147,6 +93,7 @@ const Navbar = () => {
 						<Menu className='h-6 w-6' />
 					</Button>
 				</div>
+				{/* End Mobile Menu and Theme Toggle */}
 			</div>
 		</div>
 	);
