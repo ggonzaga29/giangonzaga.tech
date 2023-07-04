a5516e3bb525b5ec750d6d33aadbe41477daf39c
Gian Gonzaga <gonzagagianfrancesco@gmail.com>
2023-07-05 03:14:04 +0800

    separate pages

---
+++ src/data/pages.ts
@@ -0,0 +1,20 @@
+const pages = [
+	{
+		name: 'Home',
+		path: '/',
+	},
+	{
+		name: 'Portfolio',
+		path: '/portfolio',
+	},
+	{
+		name: 'Contact',
+		path: '/contact',
+	},
+	{
+		name: 'Blog',
+		path: '/blog',
+	},
+]
+
+export default pages;
