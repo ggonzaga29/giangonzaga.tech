d9df0fbde9bdb2fc3dae4b9a4e80e6f9afa80bb8
Gian Gonzaga <gonzagagianfrancesco@gmail.com>
2023-07-05 03:03:02 +0800

    add drawer animations

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -49,7 +49,7 @@ const Navbar = () => {
 	];
 
 	return (
-		<div className='h-16 md:mt-10 mt-5 select-none'>
+		<div className='h-16 mt-2 md:mt-10 select-none'>
 			<div className='md:max-w-[67%] mx-auto md:px-0 px-4 h-full flex items-center justify-between'>
 				<div>
 					<h1 className='font-bold text-3xl '>
@@ -83,14 +83,16 @@ const Navbar = () => {
 
 					{/* drawer */}
 					<div
-						className={`${isSidebarOpen ? '' : 'hidden'} md:hidden`}
+						// className={`${isSidebarOpen ? '' : 'hidden'} md:hidden`}
 					>
 						{/* overlay */}
 						<div
-							className='absolute w-full h-full inset-0 bg-black bg-opacity-50 z-50 transition-all duration-200'
+							className={`absolute w-full h-full inset-0 bg-black bg-opacity-50 z-50 transition-all duration-200 ${isSidebarOpen ? '' : 'opacity-0 pointer-events-none'}`}
 							onClick={() => setIsSidebarOpen(!isSidebarOpen)}
 						/>
-						<div className='absolute w-1/2 left-0 top-0 bg-white dark:bg-zinc-950 z-50 h-full'>
+						<div className={`absolute w-2/3 left-0 top-0 bg-white dark:bg-zinc-950 z-50 h-full transition-all duration-200 ${
+							isSidebarOpen ? '' : 'transform -translate-x-full'
+						}`}>
 							{/* header */}
 							<div className='flex justify-between items-center px-2 py-2'>
 								<h1 className='font-bold text-3xl'>
@@ -107,23 +109,23 @@ const Navbar = () => {
 								<Button
 									variant='outline'
 									size='icon'
-									className='flex items-center justify-center absolute left-[110%]'
+									className={`flex items-center justify-center absolute left-[110%] delay-100 transition-all duration-50 ${isSidebarOpen ? '' : 'transform -translate-y-[120%] opacity-0'}`}
 									onClick={() => setIsSidebarOpen(!isSidebarOpen)}
 								>
 									<X className='h-6 w-6' />
 								</Button>
 							</div>
 							{/* end header */}
-							<div className='flex flex-col'>
+							<div className='flex flex-col mt-3'>
 								{pages.map((page) => (
 									<Link
 										href={page.path}
 										key={nanoid()}
 										className={`${
 											currentRoute === page.path
-												? 'bg-gray-100 hover:bg-gray-200 dark:bg-gray-800 dark:hover:bg-gray-700'
+												? 'bg-gray-100 hover:bg-gray-200 dark:bg-zinc-800 dark:hover:bg-zinc-700'
 												: ''
-										} transition-all duration-300 px-2 py-2`}
+										} transition-all duration-300 px-4 py-4`}
 									>
 										{page.name}
 									</Link>
@@ -131,6 +133,7 @@ const Navbar = () => {
 							</div>
 						</div>
 					</div>
+					{/* drawer end */}
 				</div>
 
 				<div className='flex gap-2 justify-center items-center'>
